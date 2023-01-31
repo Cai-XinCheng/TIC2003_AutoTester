9f94769531b5ac298a2be86022649a091fe3c920
Cai-XinCheng <e0895819@u.nus.edu>
2023-01-31 21:30:55 +0800

    Complete generateSouceCode

--- Tester.java
+++ Tester.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 /*
@@ -14,7 +17,7 @@ import java.util.Random;
 public class Tester {
 
     // Number of testcases to be generated and tested
-    private static final int caseCount = 1;
+    private static final int CASECOUNT = 1;
 
     // Path of generated sourceCode
     private static final String sourceCodePath = "C:\\Users\\Smile\\Documents\\XinCheng\\TIC2003\\Tester\\sourceCode";
@@ -25,19 +28,64 @@ public class Tester {
     // Path of AutoTester.exe
     private static final String testerPath = "C:\\Users\\Smile\\Documents\\XinCheng\\TIC2003\\SPA\\Code\\Debug\\AutoTester.exe";
 
+    
+    private Random random = new Random();
     // List to store all generated sourceCode
     private List<String> sourceCode;
     // List to store all generated queries
     private List<String> queries;
-    private Random random = new Random();
+    private List<String> procedures;
+    private List<String> variables;
+    private List<String> constants;
+    private Map<Integer ,String> statements;
     
     public Tester() {
         sourceCode = new ArrayList<>();
         queries = new ArrayList<>();
+        queries = new ArrayList<>();
+        procedures = new ArrayList<>();
+        variables = new ArrayList<>();
+        constants = new ArrayList<>();
+        statements = new HashMap<>();
     }
 
     public void generateSourceCode() {
-        
+        // Procedure
+        String procedureName = getName();
+        sourceCode.add("procedure");
+        sourceCode.add(procedureName);
+        sourceCode.add("{");
+        procedures.add(procedureName);
+
+        // Statements
+        // Random number of stataments
+        int noOfStatment = 10;
+        // Statement types
+        List<String> stmtTypes = new ArrayList<>(Arrays.asList("read", "print", "assign"));
+        for (int i = 0; i < noOfStatment; i++) {
+            String stmtType = stmtTypes.get(random.nextInt(stmtTypes.size()));
+            if (stmtType.equals("read")) {
+                sourceCode.add("read");
+                String variableName = getName();
+                sourceCode.add(variableName);
+                sourceCode.add(";");
+                variables.add(variableName);
+            }
+            else if (stmtType.equals("print")) {
+                if (variables.isEmpty()) {
+                    i--;
+                    break;
+                }
+                sourceCode.add("print");
+                sourceCode.add(variables.get(random.nextInt(variables.size())));
+                sourceCode.add(";");
+            }
+            else if (stmtType.equals("assign")) {
+                getAssignStatement();
+            }
+            statements.put(i+1, stmtType);
+        }
+        sourceCode.add("}");
     }
 
     public void generateQueries() {
@@ -48,9 +96,86 @@ public class Tester {
 
     }
 
+    // A function to generate randomized procedure/variable name
+    // Name format: [a-zA-Z][a-zA-Z0-9]*
+    public String getName() {
+        int MAX_LENGTH = 50;
+        String alphbets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String digits = "0123456789";
+        String characters = alphbets + digits;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(alphbets.charAt(random.nextInt(alphbets.length())));
+        for (int i = 0; i < random.nextInt(MAX_LENGTH); i++) {
+            sb.append(characters.charAt(random.nextInt(characters.length())));
+        }
+
+        return sb.toString();
+    }
+
+    // A function to generate random integer
+    public Integer getConstant() {
+        return random.nextInt();
+    }
+
+    public void getAssignStatement() {
+        // LHS = RHS
+        // LHS: true - declared variable, false - new varible
+        // RHS: true - declared variable, false - constant
+        
+        boolean lhs = random.nextBoolean();
+        boolean rhs = random.nextBoolean();
+        String variableName1;
+        String variableName2;
+        if (lhs && !variables.isEmpty()) {
+            variableName1 = variables.get(random.nextInt(variables.size()));
+            sourceCode.add(variableName1);
+        }
+        else {
+            variableName1 = getName();
+            sourceCode.add(variableName1);
+        }
+        sourceCode.add("=");
+        if (rhs && !variables.isEmpty()) {
+            variableName2 = variables.get(random.nextInt(variables.size()));
+            if (variableName1.equals(variableName2)) {
+                sourceCode.add(getConstant().toString());
+            }
+            else {
+                sourceCode.add(variableName2);
+            }
+        }
+        else {
+            sourceCode.add(getConstant().toString());
+        }
+        sourceCode.add(";");
+    }
+
+    public void printSouceCode() {
+        StringBuilder sb = new StringBuilder();
+        for (String string : sourceCode) {
+            if (string.equals("procedure")) {
+                sb.append(string);
+            }
+            else if (string.equals("{") || string.equals(";")) {
+                sb.append(string);
+                sb.append("\n    ");
+            }
+            else if (string.equals("}")) {
+                sb.delete(sb.length()-4, sb.length());
+                sb.append(string);
+            }
+            else {
+                sb.append(" ");
+                sb.append(string);
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
     public static void main(String[] args) {
 
-        for (int i = 0; i < caseCount; i++) {
+        for (int i = 0; i < CASECOUNT; i++) {
             // Init
             Tester tester = new Tester();
             // Generate source code
@@ -59,6 +184,8 @@ public class Tester {
             tester.generateQueries();
             // Execute and test
             tester.test();
+
+            tester.printSouceCode();
         }
     }
 }
